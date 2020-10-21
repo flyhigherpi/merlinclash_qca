4c4d915ddbb78397668327a4da962b7f4e721e9a
flyhigherpi <67776691+flyhigherpi@users.noreply.github.com>
2020-10-21 16:38:40 +0800

    U

---
+++ README.md
@@ -0,0 +1,2 @@
+# merlinclash_qca
+顾名思义_qca
---
+++ config.json.js
@@ -0,0 +1,12 @@
+{
+"build_date":"2020-10-21_15:01:42",
+"description":"Merlin Clash",
+"home_url":"Module_merlinclash.asp",
+"name":"merlinclash",
+"title":"Merlin Clash",
+"version":"1021999",
+"patch_version":"000",
+"sc_version":"2020090801",
+"com_version":"2020081701",
+"game_version":"2020071901"
+}
